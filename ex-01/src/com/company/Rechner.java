cffe720b8b2cee314b87c005a621c0e538870a6c
Andreas Willi <andiwilli8460@gmail.com>
2019-02-18 13:38:14 +0100

    Add ex-01

---
+++ ex-01/src/com/company/Rechner.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Rechner {
+    public static void main(String[] args) {
+
+        // b)
+        double fzA = 5;
+        double fzB = 15;
+        double vA = 80;
+        double vB = 70;
+        System.out.println("Geschwindigkeit A: "+vA);
+        System.out.println("Geschwindigkeit B: "+vB);
+        System.out.println("Länge A: "+fzA);
+        System.out.println("Länge B: "+fzB);
+
+        // c)
+        double sA = vA / 2;
+        double sB = vB / 2;
+        System.out.println("Sicherheitsabstand 1: " + sA);
+        System.out.println("Sicherheitsabstand 2: " + sB);
+
+        // d)
+        double streckeRelativ = fzA + vA / 2 + fzB + vB / 2;
+        System.out.println("Relative Strecke: " + streckeRelativ + " Meter");
+
+        // e)
+        double vRelativ = vA - vB;
+        System.out.println("Relative Geschwindigkeit: " + vRelativ);
+
+        // f)
+        double t = streckeRelativ * 3.6 / vRelativ;
+        System.out.println("Benötigte Zeit: " + t + " Sekunden");
+
+        // g)
+        double ueberholweg = t * vA / 3.6;
+        System.out.println("Ueberholweg: " + ueberholweg + " Meter");
+    }
+}
