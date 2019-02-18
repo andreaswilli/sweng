e781981c2da305ccc853d14341ebe73920d11f3b
Andreas Willi <andiwilli8460@gmail.com>
2019-02-18 19:13:27 +0100

    Clean up and add ex-02

---
+++ 02-offertenvergleich/src/Offertenvergleich.java
@@ -0,0 +1,9 @@
+public class Offertenvergleich {
+  public static void main(String[] args) {
+    double preisA; // Preis Lieferant A
+    double preisB; // Preis Lieferant B
+    int lZeitA; // Lieferzeit Lieferant A
+    int lZeitB; // Lieferzeit Lieferant B
+    // TODO: Implementation
+  }
+}
